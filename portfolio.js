5ed34e59b549f161325bc71a4319e5dce782952b
Arunkumarasamy <142664744+Arunkumarasamy@users.noreply.github.com>
2024-02-27 18:23:06 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,26 @@
+function home(){
+    var ho=document.getElementById("home")
+    ho.scrollIntoView({behavior:"smooth"})
+}
+function skills(){
+    var sk=document.getElementById("skill")
+    sk.scrollIntoView({behavior:"smooth"})
+}
+function about(){
+    var ab =document.getElementById("about")
+    ab.scrollIntoView({behavior:"smooth"})
+}
+var project=()=>{
+    var pro =document.getElementById("project")
+    pro.scrollIntoView({behavior:"smooth"})
+}
+var contact=()=>{
+    var con = document.getElementById("contact")
+    con.scrollIntoView({behavior:"smooth"})
+}
+var arrow_icon=()=>{
+    var ai=document.getElementById("home")
+    ai.scrollIntoView({behavior:"smooth"})
+}
+
+// "https://www.linkedin.com/in/arun-k-2b8536239"
